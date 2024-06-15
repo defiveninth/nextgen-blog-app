0f4edf44a894e204264e38a3a877371214357531
dearm762 <misterfighter1990@gmail.com>
2024-06-15 10:17:54 -0700

    sign in data fetched

--- components/auth/client/sign-in.form.tsx
+++ components/auth/client/sign-in.form.tsx
@@ -1,22 +1,77 @@
+'use client'
+
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { ArrowRight } from 'lucide-react'
 
 const SignInForm = () => {
+  const [formData, setFormData] = useState({ eu: '', password: '' })
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setIsLoading(true)
+    setError('')
+
+    try {
+      const res = await fetch('http://localhost:5000/auth/sign-in', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: formData.eu, password: formData.password }),
+      })
+
+      const data = await res.json()
+      if (data.user) {
+        console.log('Sign-in successful:', data)
+      } else {
+        setError(data.message)
+        console.error('Sign-in failed:', data.message)
+      }
+    } catch (err) {
+      setError('An error occurred during sign-in. Please try again.')
+      console.error('Sign-in error:', err)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
-    <form className='mx-auto mt-16 flex flex-col gap-5'>
+    <form onSubmit={handleSubmit} className='mx-auto mt-16 flex flex-col gap-5'>
       <input
         type="text"
+        name="eu"
         placeholder="Enter your email or username:"
+        value={formData.eu}
+        onChange={handleChange}
         className="py-4 px-2 w-72 outline-none transition-colors duration-500 border-b-transparent focus:border-b-white border-b-2"
       />
       <input
         type="password"
+        name="password"
         placeholder="Enter your password:"
+        value={formData.password}
+        onChange={handleChange}
         className="py-4 px-2 w-72 outline-none transition-colors duration-500 border-b-transparent focus:border-b-white border-b-2"
       />
-      <button className="btn btn-outline">
-        <span>Sign In</span>
-        <ArrowRight size={19} className='ml-[-5px]' />
+      <button type="submit" className="btn btn-outline" disabled={isLoading}>
+        {isLoading ? 'Signing In...' : (
+          <>
+            <span>Sign In</span>
+            <ArrowRight size={19} className='ml-[-5px]' />
+          </>
+        )}
       </button>
+      {error && <p className="text-red-500">{error}</p>}
     </form>
   )
 }
